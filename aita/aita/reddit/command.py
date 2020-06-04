37d53979539f5661b6a053033aea7b4c08166c23
Logan Connolly <logan.connolly@protonmail.com>
2020-06-04 23:44:29 +0200

    Change how connection to reddit is made

--- aita/aita/reddit/command.py
+++ aita/aita/reddit/command.py
@@ -1,29 +1,27 @@
 from collections import Counter
 
 import typer
-
 from tqdm import tqdm
 
-from .connect import connect_reddit
 from .posts import RedditPosts
 from .style import styled_add
-from ..config import reddit_settings
 
-reddit = connect_reddit(reddit_settings)
+
 app = typer.Typer()
 
 
 @app.command()
-def add(n_posts: int, category: str = "top") -> None:
+def add(host: str, n_posts: int, category: str = "top") -> None:
     """Add AITA reddit posts to database via api.
     :param n_posts: how many posts to add to database
     :param category: choose which filter to apply to subreddit
     """
-    rp = RedditPosts(reddit)
+    config_file = typer.prompt("Path to config")
+    rp = RedditPosts(config_file)
     posts = rp.get_posts(n_posts, category)
 
     count = Counter()
     for _, post in tqdm(enumerate(posts)):
-        tag = rp.tag_post_status(rp, post)
+        tag = rp.tag_post_status(host, post)
         count[tag] += 1
     styled_add(count)
