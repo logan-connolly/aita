fbba7c30f9187a8d97cbc0c85fd1574fac12b042
Logan Connolly <logan.connolly@protonmail.com>
2020-06-08 19:49:42 +0200

    Update endpoints to use proper schema and add params to get_posts endpoint

--- api/app/api/api_v1/endpoints/posts.py
+++ api/app/api/api_v1/endpoints/posts.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 from asyncpg.exceptions import UniqueViolationError
@@ -17,27 +18,37 @@ router = APIRouter()
 
 
 @router.get("/", response_model=List[schemas.PostDB], status_code=HTTP_200_OK)
-async def get_posts(label: str = None):
-    posts = await (
-        models.Post.objects.filter(label__contains=label).all()
-        if label
-        else models.Post.objects.all()
-    )
+async def get_posts(label: str = None, n: int = 5):
+    """Get list of of AITA posts.
+    :param label: filter by AITA label
+    :param n: limit the number of posts returned
+    """
+    posts = await models.Post.objects.all()
     if not posts:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No posts found")
-    return posts
+    if label:
+        posts = [post for post in posts if post.label == label]
+    sample_size = min(len(posts), n)
+    return random.sample(posts, sample_size)
 
 
 @router.post("/", response_model=schemas.PostDB, status_code=HTTP_201_CREATED)
 async def add_post(payload: schemas.PostCreate):
+    """Add AITA post to database.
+    :param payload: id, label, text, title
+    """
     try:
-        return await models.Post.objects.create(**payload.dict())
+        await models.Post.objects.create(**payload.dict())
+        return payload
     except UniqueViolationError:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Post exists")
 
 
 @router.get("/{id}/", response_model=schemas.PostDB, status_code=HTTP_200_OK)
-async def get_post(id: int):
+async def get_post(id: str):
+    """Retrieve AITA post by id.
+    :param id: id assigned to AITA post by reddit
+    """
     try:
         return await models.Post.objects.get(id=id)
     except NoMatch:
@@ -45,7 +56,11 @@ async def get_post(id: int):
 
 
 @router.put("/{id}/", response_model=schemas.PostDB, status_code=HTTP_200_OK)
-async def update_post(id: int, payload: schemas.PostUpdate):
+async def update_post(id: str, payload: schemas.PostUpdate):
+    """Update attributes of AITA post.
+    :param id: id assigned to AITA post by reddit
+    :param payload:
+    """
     post = await get_post(id)
     payload = {k: v for k, v in payload.dict().items() if v is not None}
     await post.update(**payload)
@@ -53,7 +68,10 @@ async def update_post(id: int, payload: schemas.PostUpdate):
 
 
 @router.delete("/{id}/", response_model=schemas.PostDB, status_code=HTTP_200_OK)
-async def remove_post(id: int):
+async def remove_post(id: str):
+    """Remove AITA posts by id.
+    :param id: id assigned to AITA post by reddit
+    """
     post = await get_post(id)
     await post.delete()
     return post
