effc65f44ba65d340afb3fed56ce3a61391503cf
Logan Connolly <logan.connolly@protonmail.com>
2021-03-26 20:32:27 +0100

    test: refactor tests to use TestClient

--- api/tests/api/test_posts.py
+++ api/tests/api/test_posts.py
@@ -1,46 +1,34 @@
 from json import dumps
 
-import pytest
+from app.core.config import settings
 
-post = dict(id="xkl123", title="AITA?", label="NTA", text="Once upon a time")
+POST = dict(id="xkl123", title="AITA?", label="NTA", text="Once upon a time")
 
 
 class TestPost:
-    @pytest.mark.asyncio
-    async def test_add_post(self, host, event_loop, server, session):
-        global post
-        async with session.post(f"{host}/posts/", data=dumps(post)) as resp:
-            assert resp.status == 201
-            response = await resp.json()
-            assert response == post
+    def test_add_post(self, client):
+        resp = client.post(f"{settings.api.version}/posts/", data=dumps(POST))
+        assert resp.status == 201
+        assert resp.json() == POST
 
-    @pytest.mark.asyncio
-    async def test_get_post(self, host, event_loop, server, session):
-        global post
-        async with session.get(f"{host}/posts/{post['id']}") as resp:
-            assert resp.status == 200
-            assert await resp.json() == post
+    def test_get_post(self, client):
+        resp = client.get(f"{settings.api.version}/posts/{POST['id']}")
+        assert resp.status == 200
+        assert resp.json() == POST
 
-    @pytest.mark.asyncio
-    async def test_get_posts(self, host, event_loop, server, session):
-        global post
-        async with session.get(f"{host}/posts/") as resp:
-            assert resp.status == 200
-            assert await resp.json() == [post]
+    def test_get_posts(self, client):
+        resp = client.get(f"{settings.api.version}/posts/")
+        assert resp.status == 200
+        assert resp.json() == [POST]
 
-    @pytest.mark.asyncio
-    async def test_update_post(self, host, event_loop, server, session):
-        global post
-        post["label"] = "YTA"
-        async with session.put(
-            f"{host}/posts/{post['id']}", data=dumps(dict(label="YTA"))
-        ) as resp:
-            assert resp.status == 200
-            assert await resp.json() == post
+    def test_update_post(self, client):
+        POST["label"] = "YTA"
+        payload = dumps(dict(label="YTA"))
+        resp = client.put(f"{settings.api.version}/posts/{POST['id']}", data=payload)
+        assert resp.status == 200
+        assert resp.json() == POST
 
-    @pytest.mark.asyncio
-    async def test_remove_post(self, host, event_loop, server, session):
-        global post
-        async with session.delete(f"{host}/posts/{post['id']}") as resp:
-            assert resp.status == 200
-            assert await resp.json() == post
+    def test_remove_post(self, client):
+        resp = client.delete(f"{settings.api.version}/posts/{POST['id']}")
+        assert resp.status == 200
+        assert resp.json() == POST
