79faa745da53c42c2ffe4325ff1141e8bef6ee3e
Logan Connolly <logan.connolly@protonmail.com>
2021-03-27 10:26:44 +0100

    tests: fix test endpoints to avoid redirect status code

--- api/tests/api/test_posts.py
+++ api/tests/api/test_posts.py
@@ -7,28 +7,28 @@ POST = dict(id="xkl123", title="AITA?", label="NTA", text="Once upon a time")
 
 class TestPost:
     def test_add_post(self, client):
-        resp = client.post(f"{settings.api.version}/posts/", data=dumps(POST))
-        assert resp.status == 201
+        resp = client.post(f"{settings.API_V1_STR}/posts/", data=dumps(POST))
+        assert resp.status_code == 201
         assert resp.json() == POST
 
     def test_get_post(self, client):
-        resp = client.get(f"{settings.api.version}/posts/{POST['id']}")
-        assert resp.status == 200
+        resp = client.get(f"{settings.API_V1_STR}/posts/{POST['id']}/")
+        assert resp.status_code == 200
         assert resp.json() == POST
 
     def test_get_posts(self, client):
-        resp = client.get(f"{settings.api.version}/posts/")
-        assert resp.status == 200
+        resp = client.get(f"{settings.API_V1_STR}/posts/")
+        assert resp.status_code == 200
         assert resp.json() == [POST]
 
     def test_update_post(self, client):
         POST["label"] = "YTA"
         payload = dumps(dict(label="YTA"))
-        resp = client.put(f"{settings.api.version}/posts/{POST['id']}", data=payload)
-        assert resp.status == 200
+        resp = client.put(f"{settings.API_V1_STR}/posts/{POST['id']}/", data=payload)
+        assert resp.status_code == 200
         assert resp.json() == POST
 
     def test_remove_post(self, client):
-        resp = client.delete(f"{settings.api.version}/posts/{POST['id']}")
-        assert resp.status == 200
+        resp = client.delete(f"{settings.API_V1_STR}/posts/{POST['id']}/")
+        assert resp.status_code == 200
         assert resp.json() == POST
