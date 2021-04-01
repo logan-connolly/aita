6541ed4c65c223a6fadad56ce07884d479b3b8af
Logan Connolly <logan.connolly@protonmail.com>
2021-04-01 09:13:22 +0200

    chore: adapt code and docker-compose to make use of debugpy server

--- api/app/core/event_handlers.py
+++ api/app/core/event_handlers.py
@@ -1,4 +1,6 @@
+import debugpy
 from fastapi import FastAPI
+from loguru import logger
 
 from app.core.config import settings
 from app.db.database import database
@@ -7,6 +9,11 @@ from app.services.reddit import get_reddit_connection, get_reddit_user
 
 
 async def start_app_handler(app: FastAPI) -> None:
+    """Services to start upon app launch"""
+    if settings.DEBUG:
+        logger.info("Debugpy server starting on port 5678")
+        debugpy.listen(("0.0.0.0", 5678))
+
     app.state.model = AITAClassifier(settings.MODEL_PATH)
     app.state.reddit = await get_reddit_connection(settings.reddit)
     app.state.user = await get_reddit_user(app.state.reddit)
@@ -14,6 +21,7 @@ async def start_app_handler(app: FastAPI) -> None:
 
 
 async def stop_app_handler(app: FastAPI) -> None:
+    """Services to stop upon app shutdown"""
     app.state.model = None
     app.state.reddit = None
     await database.disconnect()
