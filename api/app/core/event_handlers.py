96ad4a8cc0dd6e23b751ec5497be64c82f30996c
Logan Connolly <logan.connolly@protonmail.com>
2021-11-12 20:55:44 +0100

    refactor: move app starting logic into event handler

--- api/app/core/event_handlers.py
+++ api/app/core/event_handlers.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
+from app.api.api_v1.routes import api_router
 from app.core.config import settings
+from app.core.middleware import add_cors_middleware
 from app.db.database import database
 from app.services.model import AITAClassifier
 from app.services.reddit import get_reddit_connection, get_reddit_user
@@ -8,9 +10,14 @@ from app.services.reddit import get_reddit_connection, get_reddit_user
 
 async def start_app_handler(app: FastAPI) -> None:
     """Services to start upon app launch"""
+
     app.state.model = AITAClassifier(settings.MODEL_PATH)
     app.state.reddit = await get_reddit_connection(settings.reddit)
     app.state.user = await get_reddit_user(app.state.reddit)
+
+    app.include_router(api_router, prefix=settings.api.version)
+    add_cors_middleware(app)
+
     await database.connect()
 
 
