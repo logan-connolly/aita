e0c22bd60621cf55881f927755a8d7ff9529fd3e
Logan Connolly <logan.connolly@protonmail.com>
2021-11-12 08:51:02 +0100

    refactor: models based on update from orm library

--- api/app/models/post.py
+++ api/app/models/post.py
@@ -1,16 +1,16 @@
 from orm import DateTime, Model, String, Text
 from sqlalchemy.sql import func
 
-from app.db.database import database, metadata
+from app.db.database import models
 
 
 class Post(Model):
-    __tablename__ = "posts"
-    __database__ = database
-    __metadata__ = metadata
-
-    id = String(primary_key=True, max_length=6, unique=True)
-    title = String(max_length=255)
-    label = String(max_length=255)
-    text = Text()
-    ts = DateTime(default=func.now())
+    tablename = "posts"
+    registry = models
+    fields = {
+        "id": String(primary_key=True, max_length=6, unique=True),
+        "title": String(max_length=255),
+        "label": String(max_length=255),
+        "text": Text(),
+        "ts": DateTime(default=func.now()),
+    }
