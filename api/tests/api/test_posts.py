dd974a42409444e54d8def89dc68af110245e2f9
Logan Connolly <logan.connolly@protonmail.com>
2021-12-07 21:35:47 +0100

    test: fix post tests

--- api/tests/api/test_posts.py
+++ api/tests/api/test_posts.py
@@ -4,19 +4,23 @@ from starlette.status import HTTP_200_OK, HTTP_201_CREATED
 
 from app.core.config import settings
 
-POST = dict(id="xkl123", title="AITA?", label="NTA", text="Once upon a time")
+POST = dict(reddit_id="xkl123", title="AITA?", label="NTA", text="Once upon a time")
+POST_ID = None
 
 
 def test_add_post(client):
     """Test that post is added to DB"""
+    global POST_ID
     resp = client.post(f"{settings.api.version}/posts/", data=dumps(POST))
+    POST_ID = resp.json()["id"]
+    POST.update({"id": POST_ID})
     assert resp.status_code == HTTP_201_CREATED
     assert resp.json() == POST
 
 
 def test_get_post(client):
     """Test that post can be retrieved from DB by id"""
-    resp = client.get(f"{settings.api.version}/posts/{POST['id']}/")
+    resp = client.get(f"{settings.api.version}/posts/{POST_ID}/")
     assert resp.status_code == HTTP_200_OK
     assert resp.json() == POST
 
@@ -25,20 +29,20 @@ def test_get_posts(client):
     """Test that a list of posts can be retrieved from DB"""
     resp = client.get(f"{settings.api.version}/posts/")
     assert resp.status_code == HTTP_200_OK
-    assert resp.json() == [POST]
+    assert any(POST == post for post in resp.json())
 
 
 def test_update_post(client):
     """Test that dummy post is properly updated"""
     POST["label"] = "YTA"
     payload = dumps(dict(label="YTA"))
-    resp = client.put(f"{settings.api.version}/posts/{POST['id']}/", data=payload)
+    resp = client.put(f"{settings.api.version}/posts/{POST_ID}/", data=payload)
     assert resp.status_code == HTTP_200_OK
     assert resp.json() == POST
 
 
 def test_remove_post(client):
     """Test that dummy post is deleted from DB"""
-    resp = client.delete(f"{settings.api.version}/posts/{POST['id']}/")
+    resp = client.delete(f"{settings.api.version}/posts/{POST_ID}/")
     assert resp.status_code == HTTP_200_OK
     assert resp.json() == POST
