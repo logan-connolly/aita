990a1bc6ed042f44f1793a3ae1af23e9cbacd9cc
Logan Connolly <logan.connolly@protonmail.com>
2021-12-21 17:38:10 +0100

    refactor: restructure endpoints

---
+++ api/app/api/api_v1/endpoints/info.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.status import HTTP_200_OK
+
+from app.models.post import Post
+from app.schemas.reddit import PostCount, RedditInfo
+
+router = APIRouter()
+
+
+@router.get("/label/", response_model=PostCount, status_code=HTTP_200_OK)
+async def get_label_counts():
+    """Check how many posts are in DB for each label."""
+    return Counter((post.label for post in await Post.objects.all()))
+
+
+@router.get("/account/", response_model=RedditInfo, status_code=HTTP_200_OK)
+async def get_connection_info(request: Request):
+    """Check whether you are connected with Reddit API."""
+    return {"user": request.app.state.user}
--- api/app/api/api_v1/routes.py
+++ api/app/api/api_v1/routes.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 
-from .endpoints import posts, predict, reddit
+from .endpoints import info, posts, predict, sync
 
 api_router = APIRouter()
 
 api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
 api_router.include_router(predict.router, prefix="/predict", tags=["predict"])
-api_router.include_router(reddit.label.router, prefix="/reddit/label", tags=["reddit"])
-api_router.include_router(reddit.info.router, prefix="/reddit/info", tags=["reddit"])
-api_router.include_router(reddit.sync.router, prefix="/reddit/sync", tags=["reddit"])
+api_router.include_router(info.router, prefix="/info", tags=["info"])
+api_router.include_router(sync.router, prefix="/sync", tags=["sync"])
