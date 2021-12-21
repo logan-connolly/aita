a9f7ea4c0798a0b7d967b84cad0d8ec45899ca74
Logan Connolly <logan.connolly@protonmail.com>
2021-12-21 19:50:38 +0100

    tests: update info and reddit endpoints

---
+++ api/tests/api/test_info.py
@@ -0,0 +1,15 @@
+from starlette.status import HTTP_200_OK
+
+from app.core.config import settings
+
+
+def test_get_reddit_info(client):
+    """Test that reddit user info is retrieved"""
+    resp = client.get(f"{settings.api.version}/info/account/")
+    assert resp.status_code == HTTP_200_OK
+
+
+def test_get_reddit_labels(client):
+    """Test that label counts are returned for the reddit posts"""
+    resp = client.get(f"{settings.api.version}/info/label/")
+    assert resp.status_code == HTTP_200_OK
--- api/tests/api/test_reddit.py
+++ api/tests/api/test_reddit.py
@@ -3,25 +3,13 @@ from starlette.status import HTTP_200_OK, HTTP_201_CREATED
 from app.core.config import settings
 
 
-def test_get_reddit_info(client):
-    """Test that reddit user info is retrieved"""
-    resp = client.get(f"{settings.api.version}/reddit/info/")
-    assert resp.status_code == HTTP_200_OK
-
-
 def test_trigger_reddit_sync(client):
     """Test that post sync is instantiated"""
-    resp = client.post(f"{settings.api.version}/reddit/sync/?filter=top&limit=50")
+    resp = client.post(f"{settings.api.version}/sync/?filter=top&limit=50")
     assert resp.status_code == HTTP_201_CREATED
 
 
 def test_get_reddit_sync_info(client):
     """Test that info regarding last sync is returned"""
-    resp = client.get(f"{settings.api.version}/reddit/sync/")
-    assert resp.status_code == HTTP_200_OK
-
-
-def test_get_reddit_labels(client):
-    """Test that label counts are returned for the reddit posts"""
-    resp = client.get(f"{settings.api.version}/reddit/label/")
+    resp = client.get(f"{settings.api.version}/sync/")
     assert resp.status_code == HTTP_200_OK
