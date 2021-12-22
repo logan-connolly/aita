1b45198bd1443a68ded064813782e7b5e609466a
Logan Connolly <logan.connolly@protonmail.com>
2021-12-22 22:18:02 +0100

    feature: add functionality for converting to spacy types

--- nlp/tests/test_transform.py
+++ nlp/tests/test_transform.py
@@ -1,3 +1,5 @@
+from spacy.tokens import Doc, DocBin
+
 from nlp import transform
 
 
@@ -8,7 +10,20 @@ def test_parse_posts(sample_posts):
 
 
 def test_split_train_data():
-    """Test that we can take list of tuples and return splits
-    input: list[tuple[str,str]]
-    output: TrainSplits"""
+    """Test that we can take list of tuples and return splits"""
     assert None
+
+
+def test_make_docs(sample_posts):
+    """Test that docs are created from list of tuples"""
+    tuple_list = transform.parse_posts(sample_posts)
+    docs = transform.make_docs(tuple_list)
+    assert all(isinstance(doc, Doc) for doc in docs)
+
+
+def test_convert_to_doc_binary(sample_posts):
+    """Test that docs are correctly written to binary format"""
+    tuple_list = transform.parse_posts(sample_posts)
+    docs = transform.make_docs(tuple_list)
+    doc_bin = transform.convert_to_doc_binary(docs)
+    assert isinstance(doc_bin, DocBin)
