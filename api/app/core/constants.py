3dcc85957b3dea898a9495275e325e371d9b967b
Logan Connolly <logan.connolly@protonmail.com>
2021-12-22 20:17:57 +0100

    chore: remove comment str mixin is correct

--- api/app/core/constants.py
+++ api/app/core/constants.py
@@ -6,8 +6,6 @@ AITA_SUBREDDIT_NAME = "AmItheAsshole"
 class AitaLabel(str, Enum):
     """Available labels from Reddit /r/AmITheAsshole"""
 
-    # TODO: check if works as well when just ineriting from Enum
-
     YTA = "YTA"
     NTA = "NTA"
     ESH = "ESH"
