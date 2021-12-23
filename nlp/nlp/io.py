23dbce019d8465b2f205a6a758f5ebc2d2af7c63
Logan Connolly <logan.connolly@protonmail.com>
2021-12-23 06:49:55 +0100

    feature: add write out functionality for docbin

--- nlp/nlp/io.py
+++ nlp/nlp/io.py
@@ -1,6 +1,10 @@
 import json
+from pathlib import Path
+
+from spacy.tokens import DocBin
 
 from nlp import paths
+from nlp.utils import get_date_stamp
 
 RawPost = dict[str, str]
 
@@ -11,3 +15,23 @@ def read_from_json_file(file_name: str) -> list[RawPost]:
     assert file_path.exists(), "Can't find file"
     text = file_path.read_text()
     return json.loads(text)
+
+
+def write_docs(doc_bin: DocBin, path: Path) -> None:
+    """Write doc binary to disk"""
+    assert path.parent.exists(), "Missing parent directory for docs"
+    doc_bin.to_disk(path)
+
+
+def write_train_docs(doc_bin: DocBin) -> None:
+    """Write train data to proper path"""
+    filename = f"{get_date_stamp()}_train.spacy"
+    file_path = paths.get_processed_data_dir() / filename
+    write_docs(doc_bin, file_path)
+
+
+def write_valid_docs(doc_bin: DocBin) -> None:
+    """Write validation data to proper path"""
+    filename = f"{get_date_stamp()}_valid.spacy"
+    file_path = paths.get_processed_data_dir() / filename
+    write_docs(doc_bin, file_path)
--- nlp/tests/test_io.py
+++ nlp/tests/test_io.py
@@ -1,6 +1,7 @@
 import pytest
+from freezegun import freeze_time
 
-from nlp import io
+from nlp import io, paths
 
 
 def test_read_from_json_file(sample_posts, sample_posts_file):
@@ -15,6 +16,17 @@ def test_read_from_json_file_does_not_exist(sample_posts):
         io.read_from_json_file("nonexistent_posts.json")
 
 
-def test_writeout_train_data():
-    """Test that we can writeout TrainSplits to disk in DocBin format"""
-    assert None
+@freeze_time("2021-12-25")
+def test_write_train_docs(monkeypatch, tmp_path, sample_doc_bin):
+    """Test that we can writeout train binary data to disk in DocBin format"""
+    monkeypatch.setattr(paths, "get_processed_data_dir", lambda: tmp_path)
+    io.write_train_docs(sample_doc_bin)
+    assert (tmp_path / "20211225_train.spacy").exists()
+
+
+@freeze_time("2021-12-25")
+def test_write_valid_docs(monkeypatch, tmp_path, sample_doc_bin):
+    """Test that we can writeout validate binary data to disk in DocBin format"""
+    monkeypatch.setattr(paths, "get_processed_data_dir", lambda: tmp_path)
+    io.write_valid_docs(sample_doc_bin)
+    assert (tmp_path / "20211225_valid.spacy").exists()
