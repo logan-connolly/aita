1e84c65962ef9aaf993cb5e6bed206ac8b08bf39
Logan Connolly <logan.connolly@protonmail.com>
2021-12-24 02:10:48 +0100

    refactor: pass run id to doc bin writeout

--- nlp/tests/test_io.py
+++ nlp/tests/test_io.py
@@ -1,6 +1,6 @@
 import pytest
 
-from nlp import io, paths
+from nlp import io, paths, utils
 
 
 def test_read_from_json_file(sample_posts, sample_posts_id):
@@ -18,12 +18,12 @@ def test_read_from_json_file_does_not_exist(sample_posts):
 def test_write_train_docs(monkeypatch, tmp_path, sample_doc_bin):
     """Test that we can writeout train binary data to disk in DocBin format"""
     monkeypatch.setattr(paths, "get_processed_data_dir", lambda: tmp_path)
-    doc_bin_path = io.write_train_docs(sample_doc_bin)
+    doc_bin_path = io.write_train_docs(sample_doc_bin, utils.generate_run_id())
     assert doc_bin_path.exists()
 
 
 def test_write_valid_docs(monkeypatch, tmp_path, sample_doc_bin):
     """Test that we can writeout validate binary data to disk in DocBin format"""
     monkeypatch.setattr(paths, "get_processed_data_dir", lambda: tmp_path)
-    doc_bin_path = io.write_valid_docs(sample_doc_bin)
+    doc_bin_path = io.write_valid_docs(sample_doc_bin, utils.generate_run_id())
     assert doc_bin_path.exists()
