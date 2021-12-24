2d3ba5a092a4d07804d067792193d5948fb2f31d
Logan Connolly <logan.connolly@protonmail.com>
2021-12-24 06:09:57 +0100

    refactor: raise request url upon fail

--- nlp/nlp/http.py
+++ nlp/nlp/http.py
@@ -8,7 +8,7 @@ PAGE_LIMIT = 50
 def get_number_of_pages(api_url: str) -> int:
     """Get number of available posts from info endpoint"""
     resp = requests.get(f"{api_url}/sync/")
-    assert resp.status_code == 200, "Request failed"
+    assert resp.status_code == 200, f"Request failed: {resp.url}"
     n_posts = int(resp.json()["posts"])
     return n_posts // PAGE_LIMIT
 
@@ -19,7 +19,7 @@ def fetch_posts(api_url: str) -> list[RawPost]:
     posts = []
     for page_number in range(1, n_pages + 1):
         resp = requests.get(f"{api_url}/posts/?page={page_number}&size={PAGE_LIMIT}")
-        assert resp.status_code == 200, "Request failed"
+        assert resp.status_code == 200, f"Request failed: {resp.url}"
         page_posts = resp.json()["items"]
         posts.extend(page_posts)
     return posts
