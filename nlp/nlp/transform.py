4912c8c5a3b314b9155a674420d7448ffe1ef576
Logan Connolly <logan.connolly@protonmail.com>
2021-12-24 02:12:13 +0100

    feature: add functionality for splitting data

--- nlp/nlp/transform.py
+++ nlp/nlp/transform.py
@@ -1,9 +1,19 @@
+import math
+import random
+from dataclasses import dataclass
+
 from spacy.tokens import Doc, DocBin
 
 from nlp.io import RawPost
 from nlp.tokenizer import nlp
 
 TextLabel = tuple[str, str]
+
+
+@dataclass
+class SplitData:
+    train: list[Doc]
+    valid: list[Doc]
 
 
 def parse_post(post: RawPost) -> TextLabel:
@@ -31,6 +41,15 @@ def make_docs(posts: list[TextLabel]) -> list[Doc]:
             doc.cats["negative"] = 1
         docs.append(doc)
     return docs
+
+
+def split_data(docs: list[Doc], train_ratio: float = 0.6) -> SplitData:
+    """Shuffle data and split into train/valid sets"""
+    assert 0 < train_ratio < 1, "Ratio must be greater than 0 and less than 1"
+    docs_copy = docs.copy()
+    random.shuffle(docs_copy)
+    cutoff = math.floor(len(docs_copy) * train_ratio)
+    return SplitData(train=docs_copy[:cutoff], valid=docs_copy[cutoff:])
 
 
 def convert_to_doc_binary(docs: list[Doc]) -> DocBin:
