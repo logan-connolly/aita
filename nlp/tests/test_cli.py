4b1d2519acd8d326809ab1e11ac3590b1e7b578d
Logan Connolly <logan.connolly@protonmail.com>
2021-12-24 19:39:29 +0100

    test: write test for downloading data

--- nlp/tests/test_cli.py
+++ nlp/tests/test_cli.py
@@ -1,4 +1,26 @@
-from nlp import cli, io
+from types import SimpleNamespace
+
+
+from nlp import cli, http, io
+
+
+def test_download(monkeypatch, sample_posts):
+    """Test that posts can be downloaded from api"""
+    api_url = "http://fakeaddress:8000/api/v1"
+
+    def mock_requests(url: str):
+        if url == f"{api_url}/sync/":
+            return SimpleNamespace(
+                status_code=200, json=lambda: {"posts": len(sample_posts)}
+            )
+        else:
+            return SimpleNamespace(
+                status_code=200, json=lambda: {"items": sample_posts}
+            )
+
+    monkeypatch.setattr(http.requests, "get", mock_requests)
+    raw_posts_path = cli.download(api_url)
+    assert raw_posts_path.exists()
 
 
 def test_preprocess(monkeypatch, tmp_path, sample_posts_id):
