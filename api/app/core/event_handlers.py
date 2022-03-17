c834b4e93a4ecf7128ed23587a46a15d443ba59c
Logan Connolly <logan.connolly@protonmail.com>
2022-03-17 22:29:01 +0100

    refactor: remove CORS and databases handler

--- api/app/core/event_handlers.py
+++ api/app/core/event_handlers.py
@@ -3,30 +3,22 @@ from fastapi_pagination import add_pagination
 
 from app.api.api_v1.routes import api_router
 from app.core.config import settings
-from app.core.middleware import add_cors_middleware
 from app.core.services.classifiers.dummy import DummyClassifier
 from app.core.services.predict import AitaPredictor
 from app.core.services.reddit import get_reddit_connection, get_reddit_user
-from app.db.database import database
 
 
 async def start_app_handler(app: FastAPI) -> None:
     """Services to start upon app launch"""
 
-    classifier = DummyClassifier()
-
-    app.state.model = AitaPredictor(classifier=classifier)
+    app.state.model = AitaPredictor(classifier=DummyClassifier())
     app.state.reddit = await get_reddit_connection(settings.reddit)
     app.state.user = await get_reddit_user(app.state.reddit)
 
     app.include_router(api_router, prefix=settings.api_version)
     add_pagination(app)
-    add_cors_middleware(app)
-
-    await database.connect()
 
 
 async def stop_app_handler(app: FastAPI) -> None:
     """Services to stop upon app shutdown"""
     await app.state.reddit.close()
-    await database.disconnect()
