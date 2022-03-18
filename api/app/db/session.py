7cb63ca364b7a2da269e2754dc1022b5c3f2ad54
Logan Connolly <logan.connolly@protonmail.com>
2022-03-18 07:35:25 +0100

    feat: add db module for fetching session on demand

---
+++ api/app/db/session.py
@@ -0,0 +1,17 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
+engine = create_async_engine(settings.async_uri)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    """Async session to inject into API endpoints"""
+    async with async_session() as session:
+        yield session
+        await session.commit()
+        await session.dispose()
